e8025537612282fdd6290cfe45cb87c3e0178adc
cedric444 <cedricpietka@gmail.com>
2020-09-17 22:01:10 +0200

    17/09/2020 soir

--- 01-PHP Cli/Exercices/Fonctions.php
+++ 01-PHP Cli/Exercices/Fonctions.php
@@ -1,28 +1,42 @@
 <?php
+// fonction pour être sûr que l'utilisateur donne un entier
+function demanderEntier($phrase) {
+    do {
+        do {
+            $nombre = readline($phrase);
+        }while(!is_numeric($nombre));
+    }while(!is_int($nombre*1));
+    return $nombre;
+}
 // fonction pour créer un tableau dont on connaît la taille
-function creerTableauTaille($valeur, $nombre) {
-    for($i=0; $i<=count($nombre); $i++) {
-        $tab1[] = $valeur[$i];
+function creerTableauTaille($tailleTableau) {
+    for($i=0;$i<$tailleTableau;$i++) {
+        $tab[]=demanderEntier("Entrer une valeur");
     }
+    return $tab;
 }
 
 // fonction pour créer un tableau dont on ne connaît pas la taille
-function creerTableau($valeur, $nombre) {
-    for($i=0; $i<=$nombre; $i++) {
-        $tab1[] = $valeur[$i];
-    }
+function creerTableauTermineParZero() {
+    do {
+        $nombre=demanderEntier("Entrer une valeur");
+        if($nombre != 0) {
+            $tab[]= $nombre;
+        }
+    }while($nombre!=0);
+        return $tab; 
 }
 
 // fonction pour afficher un tableau (avec for)
-function afficherTableau($tab1) {
-    for($i=0; $i<=count($tab1); $i++) {
-        echo $tab1[i];
+function afficherTableau($tab) {
+    for($i=0; $i<=count($tab); $i++) {
+        echo $tab[$i]."\t";
     }
 }
 
 // fonction pour afficher un tableau (avec foreach)
 function afficherTableau2($tab1) {
     foreach($tab1 as $elt) {
-        echo $elt;
+        echo $elt."\t";
     }
 }
