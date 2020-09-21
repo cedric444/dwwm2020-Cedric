3e15beedc5d5b76f1302c1a04ae3862999020baa
cedric444 <cedricpietka@gmail.com>
2020-09-21 17:18:06 +0200

    21/09/2020 17h18

--- 01-PHP Cli/Exercices/6.11 avec fonctions.php
+++ 01-PHP Cli/Exercices/6.11 avec fonctions.php
@@ -1,7 +1,15 @@
 <?php
 require('Fonctions.php');
 $nombreValeurs = demanderEntier("Combien de valeurs contiendra le tableau 1? ");
-$array1 = creerTableauTaille("Entrer les valeurs du tableau 1: ");
+$array1 = creerTableauTaille($nombreValeurs);
 $nombreValeurs2 = demanderEntier("Combien de valeurs contiendra le tableau 2? ");
-$array2 = creerTableauTaille("Entrer les valeurs du tableau 2: ");
-for
+$array2 = creerTableauTaille($nombreValeurs2);
+$schtroumpf = 0;
+for($i=0; $i<$nombreValeurs2; $i++)
+{
+     foreach( $array1 as $elt)
+     {
+        $schtroumpf += $array2[$i] * $elt;          
+     }
+}
+echo $schtroumpf."\n"; 
