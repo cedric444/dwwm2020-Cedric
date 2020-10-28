1f3f26b678bee04ec83f49fba98882bd360b06f4
59011-95-10 <59011-95-10@exchange.ad.afpanet>
2020-10-28 17:19:49 +0100

    28/10/2020 17:19:49,15

--- BDD/Backup/Backup_exercice3_20201028.sql
+++ BDD/Backup/Backup_exercice3_20201028.sql
@@ -35,6 +35,8 @@ CREATE TABLE `avoir_note` (
   `idEtudiant` int(11) DEFAULT NULL,
   `idEpreuve` int(11) DEFAULT NULL,
   `note` int(11) DEFAULT NULL,
+  `dateCreation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dateModification` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`idAvoirNote`),
   KEY `FK_AvoirNote_Epreuves` (`idEpreuve`),
   KEY `FK_AvoirNote_Etudiants` (`idEtudiant`),
@@ -49,7 +51,7 @@ CREATE TABLE `avoir_note` (
 
 LOCK TABLES `avoir_note` WRITE;
 /*!40000 ALTER TABLE `avoir_note` DISABLE KEYS */;
-INSERT INTO `avoir_note` VALUES (1,1,1,15),(2,2,1,8),(3,3,1,7),(4,4,1,11),(5,5,1,15),(6,6,1,16),(7,7,1,1),(8,17,1,6),(9,18,1,11),(10,1,2,12),(11,2,2,12),(12,3,2,3),(13,4,2,15),(14,5,2,9),(15,6,2,11),(16,7,2,13),(17,17,2,19),(18,18,2,6),(19,8,3,8),(20,9,3,14),(21,10,3,14),(22,11,3,11),(23,12,3,6),(24,13,3,3),(25,14,3,20),(26,15,3,12),(27,16,3,11),(28,8,4,7),(29,9,4,11),(30,10,4,12),(31,11,4,3),(32,12,4,20),(33,13,4,12),(34,14,4,10),(35,15,4,8),(36,16,4,10),(37,17,4,8);
+INSERT INTO `avoir_note` VALUES (1,1,1,15,'2020-10-28 13:46:04',NULL),(2,2,1,8,'2020-10-28 13:46:04',NULL),(3,3,1,7,'2020-10-28 13:46:04',NULL),(4,4,1,11,'2020-10-28 13:46:04',NULL),(5,5,1,15,'2020-10-28 13:46:04',NULL),(6,6,1,16,'2020-10-28 13:46:04',NULL),(7,7,1,1,'2020-10-28 13:46:04',NULL),(8,17,1,6,'2020-10-28 13:46:04',NULL),(9,18,1,11,'2020-10-28 13:46:04',NULL),(10,1,2,12,'2020-10-28 13:46:04',NULL),(11,2,2,12,'2020-10-28 13:46:04',NULL),(12,3,2,3,'2020-10-28 13:46:04',NULL),(13,4,2,15,'2020-10-28 13:46:04',NULL),(14,5,2,9,'2020-10-28 13:46:04',NULL),(15,6,2,11,'2020-10-28 13:46:04',NULL),(16,7,2,13,'2020-10-28 13:46:04',NULL),(17,17,2,19,'2020-10-28 13:46:04',NULL),(18,18,2,6,'2020-10-28 13:46:04',NULL),(19,8,3,8,'2020-10-28 13:46:04',NULL),(20,9,3,14,'2020-10-28 13:46:04',NULL),(21,10,3,14,'2020-10-28 13:46:04',NULL),(22,11,3,11,'2020-10-28 13:46:04',NULL),(23,12,3,6,'2020-10-28 13:46:04',NULL),(24,13,3,3,'2020-10-28 13:46:04',NULL),(25,14,3,20,'2020-10-28 13:46:04',NULL),(26,15,3,12,'2020-10-28 13:46:04',NULL),(27,16,3,11,'2020-10-28 13:46:04',NULL),(28,8,4,7,'2020-10-28 13:46:04',NULL),(29,9,4,11,'2020-10-28 13:46:04',NULL),(30,10,4,12,'2020-10-28 13:46:04',NULL),(31,11,4,3,'2020-10-28 13:46:04',NULL),(32,12,4,20,'2020-10-28 13:46:04',NULL),(33,13,4,12,'2020-10-28 13:46:04',NULL),(34,14,4,10,'2020-10-28 13:46:04',NULL),(35,15,4,8,'2020-10-28 13:46:04',NULL),(36,16,4,10,'2020-10-28 13:46:04',NULL),(37,17,4,8,'2020-10-28 13:46:04',NULL);
 /*!40000 ALTER TABLE `avoir_note` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -71,6 +73,8 @@ CREATE TABLE `enseignants` (
   `telephoneEnseignant` varchar(14) DEFAULT NULL,
   `dateNaissanceEnseignant` date DEFAULT NULL,
   `dateEmbaucheEnseignant` date DEFAULT NULL,
+  `dateCreation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dateModification` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`idEnseignant`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -81,7 +85,7 @@ CREATE TABLE `enseignants` (
 
 LOCK TABLES `enseignants` WRITE;
 /*!40000 ALTER TABLE `enseignants` DISABLE KEYS */;
-INSERT INTO `enseignants` VALUES (1,'TALON','ISABELLE','MAITRE DE CONFERENCES','12,rue des lilas','marseille',13000,'29-89-76-30','1965-05-30','1991-10-01'),(2,'PELLETIER','SÉVERINE','CERTIFIE','213,avenue de londres','calais',62100,'21-54-87-61','1975-04-21','2014-09-01'),(3,'ROSEAU','ALAIN','AGREGE','12,allee des mimosas','calais',62100,'21-65-87-43','1960-01-02','1991-10-01'),(4,'PREUX','ERICK','CERTIFIE','76,rue charles de gaulle','lyon',69000,'30-87-21-54','1969-11-09','1995-10-01'),(5,'ROUSSEL','PHILIPPE','MAITRE DE CONFERENCES','43,rue des cogognes','lille',59000,'28-90-86-64','1966-01-21','1990-10-12'),(6,'RENAUD','LEON','MAITRE DE CONFERENCES','34,allee luoia','lille',59000,'28-29-30-31','1968-12-12','1994-10-10'),(7,'DELIGNIERES','BENEDICTE','MAITRE DE CONFERENCES','124,allee rouids','lyon',69000,'45-87-91-03','1964-10-13','1991-10-01'),(8,'ROBILLARD','MARCEL','AGREGE','12,route de paris','lille',59000,'28-28-39-39','1965-12-12','1995-10-01'),(9,'SAVASTA','SOPHIE','CERTIFIE','32,rue luois david','calais',62100,'21-78-64-54','1959-10-09','1996-10-01'),(10,'CAYRON','ISABELLE','AGREGE','56,rue de majorettes','lille',59000,'28-98-59-01','1965-09-09','1993-10-01'),(11,'PACOU','ALAIN','AGREGE','34,rue monsigny','saint omer',62300,'21-94-63-20','1978-12-01','1998-10-01');
+INSERT INTO `enseignants` VALUES (1,'TALON','ISABELLE','MAITRE DE CONFERENCES','12,rue des lilas','marseille',13000,'29-89-76-30','1965-05-30','1991-10-01','2020-10-28 13:46:40',NULL),(2,'PELLETIER','SÉVERINE','CERTIFIE','213,avenue de londres','calais',62100,'21-54-87-61','1975-04-21','2014-09-01','2020-10-28 13:46:40',NULL),(3,'ROSEAU','ALAIN','AGREGE','12,allee des mimosas','calais',62100,'21-65-87-43','1960-01-02','1991-10-01','2020-10-28 13:46:40',NULL),(4,'PREUX','ERICK','CERTIFIE','76,rue charles de gaulle','lyon',69000,'30-87-21-54','1969-11-09','1995-10-01','2020-10-28 13:46:40',NULL),(5,'ROUSSEL','PHILIPPE','MAITRE DE CONFERENCES','43,rue des cogognes','lille',59000,'28-90-86-64','1966-01-21','1990-10-12','2020-10-28 13:46:40',NULL),(6,'RENAUD','LEON','MAITRE DE CONFERENCES','34,allee luoia','lille',59000,'28-29-30-31','1968-12-12','1994-10-10','2020-10-28 13:46:40',NULL),(7,'DELIGNIERES','BENEDICTE','MAITRE DE CONFERENCES','124,allee rouids','lyon',69000,'45-87-91-03','1964-10-13','1991-10-01','2020-10-28 13:46:40',NULL),(8,'ROBILLARD','MARCEL','AGREGE','12,route de paris','lille',59000,'28-28-39-39','1965-12-12','1995-10-01','2020-10-28 13:46:40',NULL),(9,'SAVASTA','SOPHIE','CERTIFIE','32,rue luois david','calais',62100,'21-78-64-54','1959-10-09','1996-10-01','2020-10-28 13:46:40',NULL),(10,'CAYRON','ISABELLE','AGREGE','56,rue de majorettes','lille',59000,'28-98-59-01','1965-09-09','1993-10-01','2020-10-28 13:46:40',NULL),(11,'PACOU','ALAIN','AGREGE','34,rue monsigny','saint omer',62300,'21-94-63-20','1978-12-01','1998-10-01','2020-10-28 13:46:40',NULL);
 /*!40000 ALTER TABLE `enseignants` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -100,6 +104,8 @@ CREATE TABLE `epreuves` (
   `dateEpreuve` date DEFAULT NULL,
   `CoefficientEpreuve` int(11) NOT NULL,
   `anneeEpreuve` int(11) DEFAULT NULL,
+  `dateCreation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dateModification` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`idEpreuve`),
   KEY `FK_epreuves_enseignants` (`idEnseignantEpreuve`),
   KEY `FK_epreuves_matieres` (`idMatiereEpreuve`),
@@ -114,7 +120,7 @@ CREATE TABLE `epreuves` (
 
 LOCK TABLES `epreuves` WRITE;
 /*!40000 ALTER TABLE `epreuves` DISABLE KEYS */;
-INSERT INTO `epreuves` VALUES (1,'interro anglais',9,1,'2014-09-12',1,1),(2,'partiel maths',3,8,'2014-09-13',3,1),(3,'partiel BD',1,2,'2014-09-18',4,2),(4,'partiel UNIX',7,3,'2014-10-01',3,2),(5,'interro BD',1,2,'2014-10-12',1,2),(6,'interro maths',3,8,'2014-10-12',4,1);
+INSERT INTO `epreuves` VALUES (1,'interro anglais',9,1,'2014-09-12',1,1,'2020-10-28 13:47:14',NULL),(2,'partiel maths',3,8,'2014-09-13',3,1,'2020-10-28 13:47:14',NULL),(3,'partiel BD',1,2,'2014-09-18',4,2,'2020-10-28 13:47:14',NULL),(4,'partiel UNIX',7,3,'2014-10-01',3,2,'2020-10-28 13:47:14',NULL),(5,'interro BD',1,2,'2014-10-12',1,2,'2020-10-28 13:47:14',NULL),(6,'interro maths',3,8,'2014-10-12',4,1,'2020-10-28 13:47:14',NULL);
 /*!40000 ALTER TABLE `epreuves` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -139,6 +145,8 @@ CREATE TABLE `etudiants` (
   `sexeEtudiant` char(1) DEFAULT NULL,
   `dateNaissanceEtudiant` date DEFAULT NULL,
   `hobby` varchar(30) DEFAULT 'sport',
+  `dateCreation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dateModification` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`idEtudiant`)
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -149,7 +157,7 @@ CREATE TABLE `etudiants` (
 
 LOCK TABLES `etudiants` WRITE;
 /*!40000 ALTER TABLE `etudiants` DISABLE KEYS */;
-INSERT INTO `etudiants` VALUES (1,'ROBLIN','LEA','12,bd de la liberte','calais',62100,'21345678','2014-09-01',1,'','F','1995-01-14','sport'),(2,'MACARTHUR','LEON','121,bd gambetta','calais',62100,'21-30-65-09','2014-09-01',1,'','M','1994-04-12','sport'),(3,'MINOL','LUC','9,rue des prairies','boulogne',62200,'21-30-20-10','2014-09-01',1,'','M','1997-03-12','sport'),(4,'BAGNOLE','SOPHIE','12,rue des capucines','wimereux',62930,'21-89-04-30','2014-09-01',1,'','F','1996-03-21','sport'),(5,'BURY','MARC','67,allee ronde','marcq',62300,'21-90-87-65','2014-09-01',1,'','M','1993-02-05','sport'),(6,'VENDRAUX','MARC','5,rue de marseille','calais',62100,'21-96-00-09','2013-09-01',1,'a redouble sa premiere annee','M','1996-01-21','sport'),(7,'VENDERMAELE','HELENE','456,rue de paris','boulogne',62200,'21-45-45-60','2014-09-01',1,'','F','1995-03-30','sport'),(8,'BESSON','LOIC','3,allee carpentier','dunkerque',59300,'28-90-89-78','2014-09-01',2,'','M','1994-05-21','sport'),(9,'GODART','JEAN-PAUL','123,rue de lens','marck',59870,'28-09-87-65','2013-09-01',2,'a double sa seconde annee','M','1993-01-12','sport'),(10,'BEAUX','MARIE','1,allee des cygnes','dunkerque',59100,'21-30-87-90','2014-09-01',2,NULL,'F','1996-04-12','sport'),(11,'TURINI','ELSA','12,route de paris','boulogne',62200,'21-32-47-97','2014-09-01',2,NULL,'F','1996-07-17','sport'),(12,'TORELLE','ELISE','123,vallee du denacre','boulogne',62200,'21-67-86-90','2014-09-01',2,NULL,'F','1997-04-16','sport'),(13,'PHARIS','PIERRE','12,avenue foch','calais',62100,'21-21-85-90','2014-09-01',2,NULL,'M','1996-03-18','sport'),(14,'EPHYRE','LUC','12,rue de lyon','calais',62100,'21-35-32-90','2014-09-01',2,NULL,'M','1995-01-21','sport'),(15,'LECLERCQ','JULES','12,allee des ravins','boulogne',62200,'21-36-71-92','2014-09-01',2,NULL,'M','1994-05-19','sport'),(16,'DUPONT','LUC','21,avenue monsigny','calais',62200,'21-21-34-99','2014-09-01',2,NULL,'M','1996-11-02','sport'),(17,'MARKE','LOIC','312,route de paris','wimereux',62930,'21-87-87-71','2014-09-01',2,NULL,'M','1996-11-12','sport'),(18,'DEWA','LEON','121,allee des eglantines','dunkerque',59100,'28-30-87-90','2014-09-01',2,NULL,'M','1997-04-03','sport');
+INSERT INTO `etudiants` VALUES (1,'ROBLIN','LEA','12,bd de la liberte','calais',62100,'21345678','2014-09-01',1,'','F','1995-01-14','sport','2020-10-28 13:47:44',NULL),(2,'MACARTHUR','LEON','121,bd gambetta','calais',62100,'21-30-65-09','2014-09-01',1,'','M','1994-04-12','sport','2020-10-28 13:47:44',NULL),(3,'MINOL','LUC','9,rue des prairies','boulogne',62200,'21-30-20-10','2014-09-01',1,'','M','1997-03-12','sport','2020-10-28 13:47:44',NULL),(4,'BAGNOLE','SOPHIE','12,rue des capucines','wimereux',62930,'21-89-04-30','2014-09-01',1,'','F','1996-03-21','sport','2020-10-28 13:47:44',NULL),(5,'BURY','MARC','67,allee ronde','marcq',62300,'21-90-87-65','2014-09-01',1,'','M','1993-02-05','sport','2020-10-28 13:47:44',NULL),(6,'VENDRAUX','MARC','5,rue de marseille','calais',62100,'21-96-00-09','2013-09-01',1,'a redouble sa premiere annee','M','1996-01-21','sport','2020-10-28 13:47:44',NULL),(7,'VENDERMAELE','HELENE','456,rue de paris','boulogne',62200,'21-45-45-60','2014-09-01',1,'','F','1995-03-30','sport','2020-10-28 13:47:44',NULL),(8,'BESSON','LOIC','3,allee carpentier','dunkerque',59300,'28-90-89-78','2014-09-01',2,'','M','1994-05-21','sport','2020-10-28 13:47:44',NULL),(9,'GODART','JEAN-PAUL','123,rue de lens','marck',59870,'28-09-87-65','2013-09-01',2,'a double sa seconde annee','M','1993-01-12','sport','2020-10-28 13:47:44',NULL),(10,'BEAUX','MARIE','1,allee des cygnes','dunkerque',59100,'21-30-87-90','2014-09-01',2,NULL,'F','1996-04-12','sport','2020-10-28 13:47:44',NULL),(11,'TURINI','ELSA','12,route de paris','boulogne',62200,'21-32-47-97','2014-09-01',2,NULL,'F','1996-07-17','sport','2020-10-28 13:47:44',NULL),(12,'TORELLE','ELISE','123,vallee du denacre','boulogne',62200,'21-67-86-90','2014-09-01',2,NULL,'F','1997-04-16','sport','2020-10-28 13:47:44',NULL),(13,'PHARIS','PIERRE','12,avenue foch','calais',62100,'21-21-85-90','2014-09-01',2,NULL,'M','1996-03-18','sport','2020-10-28 13:47:44',NULL),(14,'EPHYRE','LUC','12,rue de lyon','calais',62100,'21-35-32-90','2014-09-01',2,NULL,'M','1995-01-21','sport','2020-10-28 13:47:44',NULL),(15,'LECLERCQ','JULES','12,allee des ravins','boulogne',62200,'21-36-71-92','2014-09-01',2,NULL,'M','1994-05-19','sport','2020-10-28 13:47:44',NULL),(16,'DUPONT','LUC','21,avenue monsigny','calais',62200,'21-21-34-99','2014-09-01',2,NULL,'M','1996-11-02','sport','2020-10-28 13:47:44',NULL),(17,'MARKE','LOIC','312,route de paris','wimereux',62930,'21-87-87-71','2014-09-01',2,NULL,'M','1996-11-12','sport','2020-10-28 13:47:44',NULL),(18,'DEWA','LEON','121,allee des eglantines','dunkerque',59100,'28-30-87-90','2014-09-01',2,NULL,'M','1997-04-03','sport','2020-10-28 13:47:44',NULL);
 /*!40000 ALTER TABLE `etudiants` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -165,6 +173,8 @@ CREATE TABLE `faire_cours` (
   `idEnseignant` int(11) DEFAULT NULL,
   `idMatiere` int(11) DEFAULT NULL,
   `annee` int(11) DEFAULT NULL,
+  `dateCreation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dateModification` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`idFaireCours`),
   KEY `FK_FaireCours_Enseignants` (`idEnseignant`),
   KEY `FK_FaireCours_Matieres` (`idMatiere`),
@@ -179,7 +189,7 @@ CREATE TABLE `faire_cours` (
 
 LOCK TABLES `faire_cours` WRITE;
 /*!40000 ALTER TABLE `faire_cours` DISABLE KEYS */;
-INSERT INTO `faire_cours` VALUES (1,1,2,2),(2,1,10,2),(3,2,4,1),(4,2,5,1),(5,2,11,1),(6,2,11,2),(7,3,8,2),(8,3,13,1),(9,4,14,1),(10,5,12,1),(11,5,12,2),(12,6,3,2),(13,6,3,1),(14,6,6,2),(15,7,13,1),(16,7,7,2),(17,7,3,2),(18,8,10,1),(19,8,13,1),(20,9,1,1),(21,9,1,2),(22,10,9,1),(23,10,9,2),(24,11,8,1);
+INSERT INTO `faire_cours` VALUES (1,1,2,2,'2020-10-28 13:48:10',NULL),(2,1,10,2,'2020-10-28 13:48:10',NULL),(3,2,4,1,'2020-10-28 13:48:10',NULL),(4,2,5,1,'2020-10-28 13:48:10',NULL),(5,2,11,1,'2020-10-28 13:48:10',NULL),(6,2,11,2,'2020-10-28 13:48:10',NULL),(7,3,8,2,'2020-10-28 13:48:10',NULL),(8,3,13,1,'2020-10-28 13:48:10',NULL),(9,4,14,1,'2020-10-28 13:48:10',NULL),(10,5,12,1,'2020-10-28 13:48:10',NULL),(11,5,12,2,'2020-10-28 13:48:10',NULL),(12,6,3,2,'2020-10-28 13:48:10',NULL),(13,6,3,1,'2020-10-28 13:48:10',NULL),(14,6,6,2,'2020-10-28 13:48:10',NULL),(15,7,13,1,'2020-10-28 13:48:10',NULL),(16,7,7,2,'2020-10-28 13:48:10',NULL),(17,7,3,2,'2020-10-28 13:48:10',NULL),(18,8,10,1,'2020-10-28 13:48:10',NULL),(19,8,13,1,'2020-10-28 13:48:10',NULL),(20,9,1,1,'2020-10-28 13:48:10',NULL),(21,9,1,2,'2020-10-28 13:48:10',NULL),(22,10,9,1,'2020-10-28 13:48:10',NULL),(23,10,9,2,'2020-10-28 13:48:10',NULL),(24,11,8,1,'2020-10-28 13:48:10',NULL);
 /*!40000 ALTER TABLE `faire_cours` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -195,6 +205,8 @@ CREATE TABLE `matieres` (
   `nomMatiere` varchar(15) NOT NULL,
   `idModule` int(11) DEFAULT NULL,
   `coefficientMatiere` int(11) DEFAULT NULL,
+  `dateCreation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dateModification` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`idMatiere`),
   KEY `FK_matieres_modules` (`idModule`),
   CONSTRAINT `FK_matieres_modules` FOREIGN KEY (`idModule`) REFERENCES `modules` (`idModule`)
@@ -207,7 +219,7 @@ CREATE TABLE `matieres` (
 
 LOCK TABLES `matieres` WRITE;
 /*!40000 ALTER TABLE `matieres` DISABLE KEYS */;
-INSERT INTO `matieres` VALUES (1,'anglais',4,2),(2,'BD',1,5),(3,'UNIX',1,5),(4,'access',1,1),(5,'bureautique',1,2),(6,'C',1,5),(7,'Prog avancee',1,3),(8,'mathematiques',2,1),(9,'expression',4,2),(10,'ACSI',1,7),(11,'economie',3,2),(12,'gestion',3,2),(13,'algorithmique',1,5),(14,'architecture',1,3);
+INSERT INTO `matieres` VALUES (1,'anglais',4,2,'2020-10-28 13:48:51',NULL),(2,'BD',1,5,'2020-10-28 13:48:51',NULL),(3,'UNIX',1,5,'2020-10-28 13:48:51',NULL),(4,'access',1,1,'2020-10-28 13:48:51',NULL),(5,'bureautique',1,2,'2020-10-28 13:48:51',NULL),(6,'C',1,5,'2020-10-28 13:48:51',NULL),(7,'Prog avancee',1,3,'2020-10-28 13:48:51',NULL),(8,'mathematiques',2,1,'2020-10-28 13:48:51',NULL),(9,'expression',4,2,'2020-10-28 13:48:51',NULL),(10,'ACSI',1,7,'2020-10-28 13:48:51',NULL),(11,'economie',3,2,'2020-10-28 13:48:51',NULL),(12,'gestion',3,2,'2020-10-28 13:48:51',NULL),(13,'algorithmique',1,5,'2020-10-28 13:48:51',NULL),(14,'architecture',1,3,'2020-10-28 13:48:51',NULL);
 /*!40000 ALTER TABLE `matieres` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -222,6 +234,8 @@ CREATE TABLE `modules` (
   `idModule` int(11) NOT NULL AUTO_INCREMENT,
   `nomModule` varchar(15) NOT NULL,
   `coefficientModule` int(11) DEFAULT NULL,
+  `dateCreation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dateModification` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`idModule`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -232,7 +246,7 @@ CREATE TABLE `modules` (
 
 LOCK TABLES `modules` WRITE;
 /*!40000 ALTER TABLE `modules` DISABLE KEYS */;
-INSERT INTO `modules` VALUES (1,'informatique',15),(2,'mathematiques',5),(3,'EOG',5),(4,'LEC',5);
+INSERT INTO `modules` VALUES (1,'informatique',15,'2020-10-28 13:49:30',NULL),(2,'mathematiques',5,'2020-10-28 13:49:30',NULL),(3,'EOG',5,'2020-10-28 13:49:30',NULL),(4,'LEC',5,'2020-10-28 13:49:30',NULL);
 /*!40000 ALTER TABLE `modules` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -245,4 +259,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-28 12:25:40
+-- Dump completed on 2020-10-28 17:15:31
