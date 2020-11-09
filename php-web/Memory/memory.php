4fef1dde9f70070f12c958a59b61e4418b8dcb6e
cedric444 <cedricpietka@gmail.com>
2020-11-09 18:06:32 +0100

    09/11/2020 18:06:32,30

--- php-web/Memory/memory.php
+++ php-web/Memory/memory.php
@@ -10,20 +10,27 @@ if (file_exists('header.php')) {
 } else {
     echo 'erreur';
 }
-
-
+/*On initialise un tableau avec les valeurs des cartes*/
+for($i=1;$i<9;$i++)
+{
+    $tab[]= $i;
+    $tab[]= $i;
+}
 for ($i=1;$i<5;$i++)
 {
     echo'<div class="ligne">';
     for($j=1;$j<5;$j++)
     {
+        $key= array_rand($tab);
+        $num= $tab[$key];
+        array_splice($tab, $key, 1);
         echo'<div class="espace"></div>
         <div class="case">
         <img  class="recto" src= "Images/plage.jpg" alt="image plage">
-        <img class="verso" src="Images/'.$i.'.jpg" alt=""></div>';
+        <img class="verso" src="Images/'.$num.'.jpg" alt=""></div>';
     }
-    echo'<div class="espaceHor"></div>';
-    echo '</div>';
+    echo'<div class="espaceHor"></div>
+    <div class="espace"></div></div>';
 }
 echo'</body>';
 echo'</html>';
