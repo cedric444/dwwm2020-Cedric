755bd28688695da62781ed052ea4965b10c4f091
cedric444 <cedricpietka@gmail.com>
2020-11-17 09:52:56 +0100

    '17/11/2020 09:53'

--- php-web/newLocalHost/Employe/exDbconnect.php
+++ php-web/newLocalHost/Employe/exDbconnect.php
@@ -20,27 +20,27 @@ catch (Exception $e){
     }
 }
 echo'on est connecté à la base de données';
-/*$requete= $db->query("SELECT idEmploye, nom, prenom FROM employes WHERE idEmploye=2");
+$requete= $db->query("SELECT idEmploye, nom, prenom FROM employes WHERE idEmploye=2");
 $reponse= $requete->fetch(PDO::FETCH_ASSOC);
 var_dump($reponse);
 $emp= new Employe($reponse);
-var_dump($emp);*/
+var_dump($emp);
 
 /**************Requetes multiples*************/
-/*$requeteM= $db->query('SELECT idEmploye, nom, prenom FROM employes');
+$requeteM= $db->query('SELECT idEmploye, nom, prenom FROM employes');
 while($donnees= $requeteM->fetch(PDO::FETCH_ASSOC))
 {
     var_dump($donnees);
     $employes[]= new Employe($donnees);
 }
-var_dump($employes);*/
+var_dump($employes);
 
 /************requete d'ajout simple************/
-/*$q= $db->exec('INSERT INTO employes(nom, prenom) VALUES("Dubois", "Leonard")');
-var_dump($q);*/
+$q= $db->exec('INSERT INTO employes(nom, prenom) VALUES("Dubois", "Leonard")');
+var_dump($q);
 
 /************requete d'ajout paramétré**********/
-/*$employe= new Employe(["nom"=>"Dufour", "prenom"=>"Patrice"]);
+$employe= new Employe(["nom"=>"Dufour", "prenom"=>"Patrice"]);
 
 $q= $db->prepare('INSERT INTO employes(nom, prenom) VALUES(:nom, :prenom)');
 
@@ -48,13 +48,19 @@ $q->bindValue(':nom', $employe->getNom());
 $q->bindValue(':prenom',$employe->getPrenom());
 
 $reponse= $q->execute();
-var_dump($reponse);*/
+var_dump($reponse);
 
 /***********requete d'ajout composé************/
-$employe= new Employe(["nom"=>"Renaud", "prenom"=>"Emilie"]);
-var_dump($employe);
-$q= $db->prepare('INSERT INTO employes(nom, prenom) VALUES('.$employe->getNom().', '.$employe->getPrenom().')');
-var_dump($q);
-var_dump($db);
-$reponse= $q->execute();
+$employe= new Employe(["nom"=>"Renaud", "prenom"=>"Emilie"]); //on crée un objet
+
+$q= $db->prepare('INSERT INTO employes(nom, prenom) VALUES("'.$employe->getNom().'", "'.$employe->getPrenom().'")'); //on prépare la requête
+
+
+$reponse= $q->execute()or die(print_r($db->errorInfo())); //on exécute la requête
 var_dump($reponse);
+
+
+/***********delete*************/
+$q = $db->exec('DELETE FROM employes WHERE prenom= "leonard"');
+var_dump($q);
+
