9a8ec9a6a1a05cfec31ce46ae872329dcce51971
cedric444 <cedricpietka@gmail.com>
2021-01-06 12:30:52 +0100

    '06/01/2021 12:31'

--- web-dynamique/Javascript/Formulaires/Formulaire v2/formulaire.php
+++ web-dynamique/Javascript/Formulaires/Formulaire v2/formulaire.php
@@ -14,42 +14,49 @@
                     <label for="nom">Nom</label>
                     <input type="text" name="nom" title="commence par une majuscule" id="nom" class="checkValidity" pattern="[A-Z]{1}[a-z]+" required>
                     <div class="image"><img src="pt_interro.png" alt="infobulle" class="info"></div>
-                    <div class="erreur"></div>
+                    <div class="erreur"><img src="valider.png" class="valider"></div>
                 </div>
                 <div class="espaceHor"></div>
                 <div class="champ">
                     <label for="prenom">Prénom</label>
-                    <input type="text" name="prenom" title="commence par une majuscule" id="prenom" class="checkValidity" pattern="[A-Z]{1}[a-z]+\-" required>
+                    <input type="text" name="prenom" title="commence par une majuscule" id="prenom" class="checkValidity" pattern="[A-Z]{1}[a-z]+(-[A-Z]{1}[a-z]+)?" required>
                     <div class="image"><img src="pt_interro.png" alt="infobulle" class="info"></div>
-                    <div class="erreur"></div>
+                    <div class="erreur"><img src="valider.png" class="valider"></div>
                 </div>
                 <div class="espaceHor"></div>
                 <div class="champ">
                     <label for="ddn">Date de Naissance</label>
-                    <input type="text" name="ddn" id="ddn" class="checkValidity" required>
+                    <input type="text" name="ddn" title="format jj/mm/aaaa" id="ddn" class="checkValidity" pattern="[0-2]{1}[0-9]{1}|[3]{1}[0-1]{1}\/[0-1]{1}[0-2]{1}\/[1-2]{1}[0|9]{1}[0-9]{2}" required>
                     <div class="image"><img src="pt_interro.png" alt="infobulle" class="info"></div>
-                    <div class="erreur"></div>
+                    <div class="erreur"><img src="valider.png" class="valider"></div>
                 </div>
                 <div class="espaceHor"></div>
                 <div class="champ">
                     <label for="codePostal">Code postal</label>
                     <input type="text" name="codePostal" title="5 chiffres attendus" id="code" class="checkValidity" pattern="\d{5}" required>
                     <div class="image"><img src="pt_interro.png" alt="infobulle" class="info"></div>
-                    <div class="erreur"></div>
+                    <div class="erreur"><img src="valider.png" class="valider"></div>
                 </div>
                 <div class="espaceHor"></div>
                 <div class="champ">
                     <label for="ville">Ville</label>
-                    <input type="text" name="ville" id="ville" class="checkValidity" required>
+                    <input type="text" name="ville" title="majuscules" id="ville" class="checkValidity" pattern="[A-Z]{2,}" required>
                     <div class="image"><img src="pt_interro.png" alt="infobulle" class="info"></div>
-                    <div class="erreur"></div>
+                    <div class="erreur"><img src="valider.png" class="valider"></div>
                 </div>
                 <div class="espaceHor"></div>
                 <div class="champ">
                     <label for="mdp">Mot de passe</label>
-                    <input type="text" name="mdp" id="mdp" class="checkValidity" required>
+                    <input type="text" name="mdp" id="mdp" class="checkValidity" pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.[!@#$%^&*+])[a-zA-z\d!@#$%^&*+]{8,}" required>
                     <div class="image"><img src="pt_interro.png" alt="infobulle" class="info"></div>
-                    <div class="erreur"></div>
+                    <div class="erreur"><img src="valider.png" class="valider"></div>
+                </div>
+                <div class="espaceHor"></div>
+                <div class="champ">
+                    <label for="verifMdp">Vérification Mot de passe</label>
+                    <input type="text" name="verifMdp" id="verifMdp" class="checkValidity" pattern="(?=.*[a-z](?=.*[A-Z])(?=.*[\d])(?=.[!@#$%^&*+])[a-zA-z\d!@#$%^&*+]{8,}" required>
+                    <div class="image"><img src="pt_interro.png" alt="infobulle" class="info"></div>
+                    <div class="erreur"><img src="valider.png" class="valider"></div>
                 </div>
                 <div class="espaceHor"></div>
                 <button>Valider</button>
--- web-dynamique/Javascript/Formulaires/Formulaire v2/script.js
+++ web-dynamique/Javascript/Formulaires/Formulaire v2/script.js
@@ -3,20 +3,44 @@
 var infos = document.getElementsByClassName("info");
 for(let i=0; i<infos.length; i++){
     infos[i].addEventListener("mouseover", (e)=>{
-        var parent= e.target;
-        var infobulle = (parent.getElementsByTagName("input")[0]).title;
-                
+        var parent= e.target.parentNode.parentNode;
+        var infobulle = (parent.getElementsByTagName("input")[0]).getAttribute("title");
+        console.log(infobulle);
+        infos[i].setAttribute("title", infobulle);         
     });
 }
 
 //Vérifier les données saisies
-inputs = document.getElementsByTagName("input");
 
-for(let i =0; i<inputs.length; i++){
-    var input = inputs[i];
-    var erreur = document.getElementsByClassName("erreur")[0];
-    if (!input.checkValidity)
+// Fonction qui va permettre de vérifier les inputs
+function verif(e)
+{
+    var input = e.target;                                           // input ciblé
+    var valider = document.getElementsByClassName("valider")[0];    // On récupère l'icone valider
+    var erreur = document.getElementsByClassName("erreur")[0];      // On récupère la div erreur
+
+    if(input.value === "")                                          // Si l'input est vide 
     {
-        erreur.textContent = "saisie incorrecte";
+        erreur.innerHTML="";
+        valider.style.visibility="hidden";
     }
+    else if (!input.checkValidity())                                //Si il y a une erreur dans l'input
+    {
+        erreur.innerHTML = "saisie incorrecte";
+        valider.src="croix.png";                                    // On modifie l'icone en croix
+        valider.style.visibility= "visible";                        
+    }
+    else                                                            // Si il n'y a pas d'erreur
+    {
+        erreur.innerHTML="";
+        valider.src= "valider.png";
+        valider.style.visibility= "visible";
+    }
+}
+
+// Pour chaque input, on vérifie la saisie
+var inputs= document.getElementsByTagName("input");                 
+for (let i=0; i<inputs.length; i++)
+{
+    inputs[i].addEventListener("input", verif);
 }
